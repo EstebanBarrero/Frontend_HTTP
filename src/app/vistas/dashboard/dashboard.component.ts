00582d3ced9a1a605bd496830a8e48536c4817a8
EstebanBarrero <barrerodavid10@hotmail.com>
2023-10-18 01:53:26 -0500

    Commit 003, TODO PAGE

--- src/app/vistas/dashboard/dashboard.component.ts
+++ src/app/vistas/dashboard/dashboard.component.ts
@@ -12,20 +12,50 @@ import {ListaUsuariosI} from '../../models/listaUsuarios.interface';
 export class DashboardComponent implements OnInit{
 
   users: ListaUsuariosI[] = [];
+  pageNumber: number = 1; // Valor inicial
 
   constructor(private api:ApiService, private router:Router ){
 
   }
 
-  //TODO hacer que this.users solo almacene el array de usuarios
-  //que este en Object.values(data) [3]
-  ngOnInit(): void {
-    this.api.getAllUsers(1).subscribe(data => {
+  cambiarNumero(numero: number) {
+    this.pageNumber = numero;
+  }
+
+
+  ngOnInit(){
+    /*this.api.getAllUsers(1).subscribe(data => {
       this.users = Object.values(data);
-      console.log(this.users[3]);
+      console.log(this.users);
     });
 
+
+
+this.api.getAllUsers(1).subscribe(data => {
+      const values = Object.values(data);
+      if (values.length >= 4) { // Verificar que el array tenga al menos 4 elementos
+        const usersArray: ListaUsuariosI[] = [values[3] as ListaUsuariosI]; // Crear un array con un único elemento
+        this.users = usersArray; // Asignar el array a this.users
+        console.log(this.users.length);
+        for (let i=0; i < this.users.length; i++){
+          console.log(this.users);
+        };
+      }*/
+
+
+    this.api.getAllUsers(this.pageNumber).subscribe(data => {
+      const values = Object.values(data);
+      if (values.length >= 4) { // Verificar que el array tenga al menos 4 elementos
+        this.users = values[3] as any;
+        console.log(this.users.length);
+        for (let i = 0; i < this.users.length; i++) {
+          console.log(this.users[i]);
+        }
+      }
+    });
   }
+
+
 
   editarUsuario(first_name: any){
     this.router.navigate(['edit', first_name]);
