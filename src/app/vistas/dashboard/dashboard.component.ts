430740260d19d45926ccbdd691943965a9f641f7
EstebanBarrero <barrerodavid10@hotmail.com>
2023-10-22 11:58:09 -0500

    Commit 005, Update Component.ts

--- src/app/vistas/dashboard/dashboard.component.ts
+++ src/app/vistas/dashboard/dashboard.component.ts
@@ -24,26 +24,7 @@ export class DashboardComponent implements OnInit{
 
 
   ngOnInit(){
-    /*this.api.getAllUsers(1).subscribe(data => {
-      this.users = Object.values(data);
-      console.log(this.users);
-    });
-
-
-
-this.api.getAllUsers(1).subscribe(data => {
-      const values = Object.values(data);
-      if (values.length >= 4) { // Verificar que el array tenga al menos 4 elementos
-        const usersArray: ListaUsuariosI[] = [values[3] as ListaUsuariosI]; // Crear un array con un único elemento
-        this.users = usersArray; // Asignar el array a this.users
-        console.log(this.users.length);
-        for (let i=0; i < this.users.length; i++){
-          console.log(this.users);
-        };
-      }*/
-
-
-    this.api.getAllUsers(this.pageNumber).subscribe(data => {
+      this.api.getAllUsers(this.pageNumber).subscribe(data => {
       const values = Object.values(data);
       if (values.length >= 4) { // Verificar que el array tenga al menos 4 elementos
         this.users = values[3] as any;
