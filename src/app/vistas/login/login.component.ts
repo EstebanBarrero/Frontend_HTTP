df3721a4307ccd622a9e20e8ad490ed3e4b589a2
Thomas <sthx09567541@gmail.com>
2023-10-22 14:33:52 -0500

    added login auth

--- src/app/vistas/login/login.component.ts
+++ src/app/vistas/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms'
-import {ApiService} from '../../services/api/api.service';
-import {LoginI} from '../../models/login.interface';
-import {Router} from '@angular/router'
-import {ResponseI} from '../../models/response.interface';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../../services/api/api.service';
+import { LoginI } from '../../models/login.interface';
+import { Router } from '@angular/router';
+import { ResponseI } from '../../models/response.interface';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit{
     password: new FormControl('', Validators.required)
   })
 
-  constructor(private api:ApiService, private router: Router){}
+  constructor(private api: ApiService, private router: Router, private http: HttpClient) { }
 
   errorStatus:boolean = false;
   errorMsj:any = "";
@@ -32,18 +33,38 @@ export class LoginComponent implements OnInit{
     }
   }
 
-  /*TODO: Activar: Login Thomas */
   onLogin(form: any) {
-    this.router.navigate(['dashboard']);
-    /*this.api.loginByEmail(form).subscribe(data =>{
-      let dataResponse:ResponseI = data;
-      if(!dataResponse.status){
-        localStorage.setItem("token", dataResponse.result.token);
-        this.router.navigate(['dashboard']);
-      }else{
-        this.errorStatus = true;
-        this.errorMsj = dataResponse.result.error_msg;
+
+    const document = form.usuario;
+    const password = form.password;
+    
+    const data = { document, password };
+  
+    this.http.post('http://127.0.0.1:8000/login', data).subscribe(
+      (response: any) => {
+        // Maneja la respuesta del servidor
+        if (response.access_token) {
+          // Si se obtiene un token de acceso, almacénalo en el almacenamiento local
+          localStorage.setItem('token', response.access_token);
+          // Redirige al usuario a la página de dashboard
+          this.router.navigate(['dashboard']);
+        } else {
+          // En caso de error, muestra un mensaje de error en el componente
+          this.errorStatus = true;
+          this.errorMsj = 'Credenciales incorrectas, intenta de nuevo';
+        }
+      },
+      (error) => {
+        if (error.status === 401) { //unauthorized (bad credentials)
+          this.errorStatus = true;
+          this.errorMsj = 'Credenciales incorrectas, intenta de nuevo.';
+        } else {  // Otro tipo de error
+          console.error('Error de conexión:', error);
+          this.errorStatus = true;
+          this.errorMsj = 'Error: Algo salió mal, intenta de nuevo Mas tarde.';
+        }
       }
-    });*/
+    );
   }
+  
 }
