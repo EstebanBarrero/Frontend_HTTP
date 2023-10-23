f7ef8c7730b0619d8664b145d2552c634b40ba17
Thomas <sthx09567541@gmail.com>
2023-10-22 21:18:02 -0500

    search

--- src/app/vistas/dashboard/dashboard.component.ts
+++ src/app/vistas/dashboard/dashboard.component.ts
@@ -1,48 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiService}from '../../services/api/api.service';
-import {Router} from '@angular/router';
+import { ApiService } from '../../services/api/api.service';
+import { Router } from '@angular/router';
 
-import {ListaUsuariosI} from '../../models/listaUsuarios.interface';
+import { ListaUsuariosI } from '../../models/listaUsuarios.interface';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit{
 
+export class DashboardComponent implements OnInit {
   users: ListaUsuariosI[] = [];
-  pageNumber: number = 2; // Valor inicial
+  pageNumber: number = 1; // Valor inicial
+  searchTerm: string = '';
+  filteredUsers: ListaUsuariosI[] = [];
 
-  constructor(private api:ApiService, private router:Router ){
-
-  }
+  constructor(private api: ApiService, private router: Router) {}
 
   cambiarNumero(numero: number) {
     this.pageNumber = numero;
   }
 
-
-  ngOnInit(){
-      this.api.getAllUsers(this.pageNumber).subscribe(data => {
+  ngOnInit() {
+    this.api.getAllUsers(this.pageNumber).subscribe((data) => {
       const values = Object.values(data);
-      if (values.length >= 4) { // Verificar que el array tenga al menos 4 elementos
+      if (values.length >= 4) {
         this.users = values[3] as any;
-        console.log(this.users.length);
-        for (let i = 0; i < this.users.length; i++) {
-          console.log(this.users[i]);
-        }
+        this.filteredUsers = this.users; // Inicialmente, muestra todos los usuarios
       }
     });
   }
 
-
-
-  editarUsuario(document: any){
+  editarUsuario(document: any) {
     this.router.navigate(['edit', document]);
   }
 
-  nuevoUsuario(){
+  nuevoUsuario() {
     this.router.navigate(['nuevo']);
+  }
+
+  onSearch() {
+    this.filteredUsers = this.users.filter((user) => {
+      return (
+        user.first_name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+        user.last_name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
+    });
   }
 }
