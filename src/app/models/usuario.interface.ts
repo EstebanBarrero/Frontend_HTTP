33da108c350852d3a4d6780f94d52124a258e378
EstebanBarrero <barrerodavid10@hotmail.com>
2023-10-23 13:35:39 -0500

    Commit 009, UserHTML

--- src/app/models/usuario.interface.ts
+++ src/app/models/usuario.interface.ts
@@ -1,4 +1,5 @@
 export interface UsuarioI{
+  id : string;
   first_name : string;
   last_name : string;
   type_document : string;
@@ -8,5 +9,6 @@ export interface UsuarioI{
   is_active : string;
   register_date : string;
   address : string;
+  user_image: string;
   role : string;
 }
--- src/app/vistas/edit/edit.component.ts
+++ src/app/vistas/edit/edit.component.ts
@@ -3,6 +3,7 @@ import { Router, ActivatedRoute} from '@angular/router';
 import {UsuarioI} from '../../models/usuario.interface';
 import {ApiService} from '../../services/api/api.service';
 import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-edit',
@@ -16,16 +17,43 @@ export class EditComponent implements OnInit{
   }
 
   datosUsuario!: UsuarioI;
-  editarFor = new FormGroup([
-    
-  ]);
+  editarForm = new FormGroup({
+    id: new FormControl(''),
+    first_name: new FormControl(''),
+    last_name: new FormControl(''),
+    type_document: new FormControl(''),
+    document: new FormControl(''),
+    birthday: new FormControl(''),
+    phone_number: new FormControl(''),
+    is_active: new FormControl(''),
+    register_date: new FormControl(''),
+    address: new FormControl(''),
+    user_image: new FormControl(''),
+    role: new FormControl('')
+});
 
   ngOnInit(): void {
     let usuarioid = this.activerouter.snapshot.paramMap.get('id');
     let token = this.getToken();
     this.api.getSigleUser(usuarioid).subscribe(data  =>{
-      console.log(data)
-    })
+      // Completar el objeto data con valores nulos o por defecto si es necesario
+    const usuarioCompleto = {
+      id: data.id,
+      first_name: data.first_name,
+      last_name: data.last_name ,
+      type_document: data.type_document,
+      document: data.document,
+      birthday: data.birthday,
+      phone_number: data.phone_number,
+      is_active: data.is_active,
+      register_date: data.register_date,
+      address: data.address,
+      user_image: data.user_image,
+      role: data.role
+    };
+    this.editarForm.setValue(usuarioCompleto); // Asignar al formulario
+    console.log(this.editarForm.value)
+  })
   }
 
   getToken(){
