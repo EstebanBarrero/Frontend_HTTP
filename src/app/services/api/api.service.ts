cc7478ac31a559b027b860061af53790ff91a1d7
Thomas Sorza <76008678+ThomasSorza@users.noreply.github.com>
2023-11-13 15:53:41 -0500

    added

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,49 +1,46 @@
 import { Injectable } from '@angular/core';
-import {LoginI} from '../../models/login.interface';
-import {ResponseI} from '../../models/response.interface';
-import {ListaUsuariosI} from '../../models/listaUsuarios.interface';
-import {UsuarioI} from '../../models/usuario.interface'
-import {ListaRolesI} from '../../models/listaRoles.interface'
-import { RolI } from 'src/app/models/rol.interface';
-import {HttpClient, HttpHeaders} from '@angular/common/http'
-import {Observable}  from 'rxjs'
+import { LoginI } from '../../models/login.interface';
+import { ResponseI } from '../../models/response.interface';
+import { ListaUsuariosI } from '../../models/listaUsuarios.interface';
+import { UsuarioI } from '../../models/usuario.interface'
+import { ListaRolesI } from '../../models/listaRoles.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  //Url en donde tengamos almacenado la api de pruebas
-  //url: string = "https://apilab3.azurewebsites.net/"
-  url: string = "http://127.0.0.1:8000/"
+  // URL donde tengamos almacenado la API
+  url: string = "http://asbackpython1.azurewebsites.net/";
   searchTerm: string = '';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  loginByEmail(form: LoginI): Observable<ResponseI>{
-    let direction = this.url
-    return this.http.post<ResponseI>(direction,form);
+  getAllUsers(page: number): Observable<ListaUsuariosI[]> {
+    let direction = `${this.url}api/users/?page=${page}`;
+    return this.http.get<ListaUsuariosI[]>(direction);
   }
 
-  getAllUsers(page:number): Observable<ListaUsuariosI[]>{
-    let direction = this.url + "api/users/?page=" + page
-    return this.http.get <ListaUsuariosI[]>(direction);
+  getBaseUrl(): string {
+    return this.url;
   }
 
-  getSigleUser(id : any): Observable<UsuarioI>{
-    let direction = this.url + "api/users/" + id + "/"
+  getSigleUser(id: any): Observable<UsuarioI> {
+    let direction = `${this.url}api/users/${id}/`;
     console.log(direction);
     return this.http.get<UsuarioI>(direction);
   }
 
-  getAllRoles(page:number): Observable<ListaRolesI[]>{ // No hay interfaz para los roles
-    let direction = this.url + "api/roles/?page=" + page
+  getAllRoles(page: number): Observable<ListaRolesI[]> {
+    let direction = `${this.url}api/roles/?page=${page}`;
     return this.http.get<ListaRolesI[]>(direction);
   }
 
-  //Metodo para realizar el Put a la base de datos por medio de la URL
-  putUser(form:UsuarioI):Observable<ResponseI>{
-    let direction = this.url + "api/users/" + form.id + "/"
-    return this.http.put<ResponseI>(direction,form);
+  // Método para realizar el Put a la base de datos por medio de la URL
+  putUser(form: UsuarioI): Observable<ResponseI> {
+    let direction = `${this.url}api/users/${form.id}/`;
+    return this.http.put<ResponseI>(direction, form);
   }
 
   setSearchTerm(term: string): void {
@@ -59,4 +56,8 @@ export class ApiService {
     return this.http.get<ListaUsuariosI[]>(direction);
   }
 
+  login(form: LoginI): Observable<ResponseI> {
+    let direction = `${this.url}login/`;  // Ajusta la URL según la estructura de tu API
+    return this.http.post<ResponseI>(direction, form);
+  }
 }
