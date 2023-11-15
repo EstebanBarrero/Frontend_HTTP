716b679d86f768b6627c89d30324be86337289f5
Thomas Sorza <76008678+ThomasSorza@users.noreply.github.com>
2023-11-15 04:03:20 -0500

    added routing urls to azure APM

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -14,7 +14,10 @@ import {Observable}  from 'rxjs'
 export class ApiService {
   //Url en donde tengamos almacenado la api de pruebas
   //url: string = "https://apilab3.azurewebsites.net/"
-  url: string = "https://asbackpython2.azurewebsites.net/"
+  url: string = "https://asbackpython3.azurewebsites.net/"
+  urlusers: string = "https://apimproject.azure-api.net/"
+  urlroles: string = "https://apimproject.azure-api.net/api/"
+  urlUserRoles: string = "https://apimproject.azure-api.net/ur/"
 
   constructor(private http:HttpClient) { }
 
@@ -29,30 +32,30 @@ export class ApiService {
   //}
 
   getSigleUser(id : any): Observable<UsuarioI>{
-    let direction = this.url + "api/users/" + id + "/"
+    let direction = this.urlusers + "api/users/" + id + "/"
     console.log(direction);
     return this.http.get<UsuarioI>(direction);
   }
 
   getAllRoles(page:number): Observable<ListaRolesI[]>{ // No hay interfaz para los roles
-    let direction = this.url + "api/roles/?page=" + page
+    let direction = this.urlroles + "api/roles/?page=" + page
     return this.http.get<ListaRolesI[]>(direction);
   }
 
   //Metodo para realizar el Put a la base de datos por medio de la URL
   putUser(form:UsuarioI):Observable<ResponseI>{
-    let direction = this.url + "api/users/" + form.id + "/"
+    let direction = this.urlusers + "api/users/" + form.id + "/"
     return this.http.put<ResponseI>(direction,form);
   }
 
   getAllUsers(searchTerm: string, page:number): Observable<ListaUsuariosI[]> {
-      let direction = this.url + "api/search-users/?page=" + page + "&search=" + searchTerm;
+      let direction = this.urlusers + "api/search-users/?page=" + page + "&search=" + searchTerm;
       console.log(direction);
       return this.http.get<ListaUsuariosI[]>(direction);
   }
 
   userRoles(): Observable<any> {
-    let direction = this.url + "api/roles_user_count/";
+    let direction = this.urlUserRoles + "api/roles_user_count/";
     return this.http.get<any>(direction);
   }
 
